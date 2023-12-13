3c952e545095af15c5d29dae9bf18e9910bca92f
Moharum1 <moharum4@gamil.com>
2023-12-13 19:16:17 +0200

    task done

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ void push(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **head);
 
 extern int global_int;
